3a4ead60893625db12e434cfd7b1bf2288ad66d2
Stanislav Bondarenko <stassss2009@gmail.com>
2025-12-25 16:57:22 +0200

    feat: add demo project

---
+++ BlazorWebApp/BlazorWebApp/Program.cs
@@ -0,0 +1,32 @@
+using BlazorWebApp.Client.Pages;
+using BlazorWebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorWebApp.Client._Imports).Assembly);
+
+app.Run();
